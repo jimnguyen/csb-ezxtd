1312b34267efd97b69f2796d31fda50d3bc4306b
Code Sandbox <hello@codesandbox.io>
2021-07-06 15:13:27 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.8f6816a61bf8ac45c3df5ae94a13f5d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2941faca307cd91b782e8279f1f79d8b",
+    "url": "https://jimnguyen.github.io/csb-ezxtd/index.html"
+  },
+  {
+    "revision": "c0ea047ae0b41fe4cff9",
+    "url": "https://jimnguyen.github.io/csb-ezxtd/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "38bdbc4c5a6a4349d90f",
+    "url": "https://jimnguyen.github.io/csb-ezxtd/static/js/2.ed72e339.chunk.js"
+  },
+  {
+    "revision": "d21b4bc0702397bf916425d3b382139b",
+    "url": "https://jimnguyen.github.io/csb-ezxtd/static/js/2.ed72e339.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0ea047ae0b41fe4cff9",
+    "url": "https://jimnguyen.github.io/csb-ezxtd/static/js/main.9c297d03.chunk.js"
+  },
+  {
+    "revision": "c85f9a46c4ce9dbb94b5",
+    "url": "https://jimnguyen.github.io/csb-ezxtd/static/js/runtime-main.4ea6282b.js"
+  }
+]);
